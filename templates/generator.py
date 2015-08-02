0fbfe63c631135b28ef0a9de5280be177659fd80
Denis Papathanasiou <denis@papathanasiou.org>
2015-08-02 14:06:21 -0400

    Updated the generator to create summary pages (preview lists of posts) based on data in the index.json 'database'

--- templates/generator.py
+++ templates/generator.py
@@ -6,10 +6,83 @@ generator.py
 This module defines a series of functions to create static html pages
 from the given templates in this folder.
 
+The overall site index and tag summary pages are drive by index.json,
+the master "database" of all posts and tags.
+
 """
 
 import sys
 from string import Template
+from datetime import datetime
+import os.path
+import json
+
+def load_text_file (filename):
+    with open (filename, "r") as f:
+        return f.read()
+
+try:
+    SITE_INDEX = json.loads(load_text_file(os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                                        '../posts/index.json')))
+except:
+    SITE_INDEX = {}
+
+#
+# index.json handling logic
+
+# post keys are strings in "%Y%m%d" format
+
+def post_date_as_datetime (s, fmt="%Y%m%d"):
+    """Convert the string in the given format to a datetime object
+       defaulting to now in YYYYMMDD format"""
+    
+    try:
+        return datetime.strptime(s, fmt)
+    except ValueError:
+        # return the current date instead
+        return datetime.utcnow()
+
+def post_date_as_link (s, fmt="%Y%m%d", docroot='..'):
+    """Convert the string in the given format to a link in /posts"""
+
+    dt = post_date_as_datetime(s, fmt)
+    return ''.join([docroot, '/posts/', dt.strftime("%Y.%m.%d"), '.post.html'])
+
+def post_date_as_display_value (s, in_fmt="%Y%m%d", out_fmt="%B %d, %Y"):
+    """Convert the string in the given input format 
+       to a string in the output (human readable) format"""
+
+    dt = post_date_as_datetime(s, in_fmt)
+    return dt.strftime(out_fmt)
+
+# each post json contains all the information to
+# produce the index or the tag summary page
+
+def posts_for_tag (tag):
+    """Return a dict of all the posts with the given tag"""
+
+    posts = {}
+    try:
+        for (date, post) in SITE_INDEX['posts'].items():
+            if tag in post['tags']:
+                posts[date] = post
+    except KeyError:
+        pass
+
+    return posts
+
+def get_tag_link (tag, docroot='..'):
+    """Return the html link for this tag"""
+
+    try:
+        tag_data = SITE_INDEX['tags'][tag]
+        return ''.join([docroot, '/', tag_data['link']])
+    except KeyError:
+        return ''
+
+
+#
+# Template generation logic
 
 template_files = {
     'PAGE': 'content.html',
@@ -17,13 +90,11 @@ template_files = {
     'HEAD': 'header.html',
     'NAV' : 'navigation.html',
     'DATE': 'post_date.html',
+    'PREV': 'post_preview.html',
+    'TAG' : 'tag.html'
 }
 
 templates = {}
-
-def load_text_file (filename):
-    with open (filename, "r") as f:
-        return f.read()
 
 for template_type, template_file in template_files.items():
     templates[template_type] = Template(load_text_file(template_file))
@@ -33,7 +104,7 @@ def generate_post (page_title, page_subtitle, page_desc, post_date, bg_image, bg
 
     heading    = templates['HEAD'].safe_substitute(title=page_title, description=page_desc, docroot='..')
     posting    = templates['DATE'].safe_substitute(post_date=post_date)
-    navigation = templates['NAV'].safe_substitute()
+    navigation = templates['NAV'].safe_substitute(docroot='..')
     footer     = templates['FOOT'].safe_substitute(docroot='..')
     
     return templates['PAGE'].safe_substitute(header=heading,
@@ -45,6 +116,53 @@ def generate_post (page_title, page_subtitle, page_desc, post_date, bg_image, bg
                                              post_date=posting,
                                              contents=load_text_file(article),
                                              footer=footer)
+
+def generate_summary (tag): 
+    """Use the SITE_INDEX to produce a summary page for this tag"""
+
+    try:
+        tag_data = SITE_INDEX['tags'][tag]
+        tag_desc = tag_data['heading']
+        
+        # get the list of posts for this tag
+        post_data = posts_for_tag (tag)
+
+        # write their summaries in reverse chronological order
+        posts = []
+        for post_date in sorted(post_data.iterkeys(), reverse=True):
+            # get the data for this post
+            post = post_data[post_date]
+            
+            # make the list of tag links
+            tags = []
+            for tag_name in post['tags']:
+                tags.append(templates['TAG'].safe_substitute(tag=tag_name,
+                                                             tag_link=get_tag_link(tag_name, docroot='')))
+
+            posts.append(templates['PREV'].safe_substitute(link=post_date_as_link(post_date),
+                                                           title=post['title'],
+                                                           sub_title=post['subtitle'],
+                                                           date=post_date_as_display_value(post_date),
+                                                           tags=', '.join(tags)))
+
+        # set the page header, footer and navigation
+        heading    = templates['HEAD'].safe_substitute(title=tag, description=tag_desc, docroot='')
+        navigation = templates['NAV'].safe_substitute(docroot='')
+        footer     = templates['FOOT'].safe_substitute(docroot='')
+
+        # write the full summary page
+        return templates['PAGE'].safe_substitute(header=heading,
+                                                 navigation=navigation,
+                                                 bg_image=tag_data['image'],
+                                                 bg_image_source=tag_data['image_src'],
+                                                 page_heading=tag,
+                                                 page_subheading=tag_desc,
+                                                 post_date='',
+                                                 contents='\n'.join(posts),
+                                                 footer=footer)
+    except IndexError:
+        pass
+    
 
 
 if __name__ == "__main__":
